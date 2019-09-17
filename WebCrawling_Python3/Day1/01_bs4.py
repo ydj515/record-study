064b5e11ae620a13340289e5adb6848dd3a7163d
ydj515 <ydj515@hanmail.net>
2019-09-17 21:27:22 +0900

    commit

--- WebCrawling_Python3/Day1/01_bs4.py
+++ WebCrawling_Python3/Day1/01_bs4.py
@@ -2,8 +2,11 @@ import bs4
 
 html_str = "<html><div></dib></html>"
 
+# html_str에 해당하는 html이 bsObj에 들어감
 bsObj = bs4.BeautifulSoup(html_str, "html.parser")
 
 print(type(bsObj))
 print(bsObj)
+
+# div 태그를 찾아줌
 print(bsObj.find("div"))
